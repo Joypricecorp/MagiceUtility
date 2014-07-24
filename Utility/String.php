f7e3456bb44fd534bd157159a604126b41e85252
liverbool <nukboon@gmail.com>
2014-07-24 18:38:29 +0700

    add #extractEmail

--- Utility/String.php
+++ Utility/String.php
@@ -49,5 +49,23 @@ namespace Magice\Utility {
             // check if error occured
             return (json_last_error() == JSON_ERROR_NONE);
         }
+
+        /**
+         * Extract emails form text
+         *
+         * @param string $text
+         *
+         * @return null|array
+         */
+        public static function extractEmail($text)
+        {
+            $pattern = "/(?:[a-z0-9!#$%&'*+=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/";
+
+            if (preg_match_all($pattern, strip_tags($text), $match)) {
+                return $match[0];
+            } else {
+                return null;
+            }
+        }
     }
 }
